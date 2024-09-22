4186db276afcbd7c1c47a5914263e5e40be8b42f
GlobalYam <aarni.hellen@gmail.com>
2024-09-22 20:53:06 +0300

    changes to envirioment variables

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = getenv("SECRET_KEY").replace("://", "ql://", 1)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace(
     "://", "ql://", 1
 )
